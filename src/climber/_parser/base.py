b52aa96f31ed2e152c8cfec3cda859f77b4dbe7c
Brody Critchlow <24994624+brodycritchlow@users.noreply.github.com>
2023-08-24 19:57:47 +0000

    Add base parser implementation

---
+++ src/climber/_parser/base.py
@@ -0,0 +1,44 @@
+from abc import ABC, abstractmethod
+from typing import Callable, Concatenate, Optional, ParamSpec, TypeVar
+
+from pydantic import BaseModel
+
+Param = ParamSpec("Param")
+RetType = TypeVar("RetType")
+OriginalFunc = Callable[Param, RetType]
+DecoratedFunc = Callable[Concatenate[str, Param], RetType]
+
+
+class ParserArgumentError(Exception):
+    ...
+
+
+ParserArgumentError.__module__ = "Climber"
+
+
+class BaseParser(BaseModel, ABC):
+    """
+    The base pydantic model for parsers, provided for ease of use.
+    Allows for the creation, and modification of more advanced models.
+    """
+
+    def parser(
+        self, name: str = "", required: Optional[bool] = False
+    ) -> Callable[[OriginalFunc], DecoratedFunc]:
+        """
+        Parser decorator, that creates the command line function.
+        """
+
+        def inner(func: OriginalFunc) -> DecoratedFunc:
+            def wrapper(*args, **kwargs):
+                annotations = func.__annotations__
+                try:
+                    __type = annotations[name]
+                except KeyError as exc:
+                    raise ParserArgumentError(
+                        f"{repr(name)} is not a valid argument, valid names: {list(annotations.keys())}"
+                    ) from exc
+
+            return wrapper
+
+        return inner
